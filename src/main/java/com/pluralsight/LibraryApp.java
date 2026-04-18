57d9b9c865080b3446fdda38de400335233acba6
TheRealRemo <timothyprice2001@gmail.com>
2026-04-17 21:03:35 -0400

    Initial commit

---
+++ src/main/java/com/pluralsight/LibraryApp.java
@@ -0,0 +1,30 @@
+package com.pluralsight;
+
+public class LibraryApp {
+    private static LibraryBookInfo[] libraryBookInfos = new LibraryBookInfo[20];
+    public static void main(String[] args) {
+
+        libraryBookInfos[1] = new LibraryBookInfo(1, "100-0000000001", "Book 1",false, "");
+        libraryBookInfos[2] = new LibraryBookInfo(1, "100-0000000002", "Book 2",false, "");
+        libraryBookInfos[3] = new LibraryBookInfo(1, "100-0000000003", "Book 3",false, "");
+        libraryBookInfos[4] = new LibraryBookInfo(1, "100-0000000004", "Book 4",false, "");
+        libraryBookInfos[5] = new LibraryBookInfo(1, "100-0000000005", "Book 5",false, "");
+        libraryBookInfos[6] = new LibraryBookInfo(1, "100-0000000006", "Book 6",false, "");
+        libraryBookInfos[7] = new LibraryBookInfo(1, "100-0000000007", "Book 7",false, "");
+        libraryBookInfos[8] = new LibraryBookInfo(1, "100-0000000008", "Book 8",false, "");
+        libraryBookInfos[9] = new LibraryBookInfo(1, "100-0000000009", "Book 9",false, "");
+        libraryBookInfos[10] = new LibraryBookInfo(1, "100-0000000010", "Book 10",false, "");
+        libraryBookInfos[11] = new LibraryBookInfo(1, "100-0000000011", "Book 11",false, "");
+        libraryBookInfos[12] = new LibraryBookInfo(1, "100-0000000012", "Book 12",false, "");
+        libraryBookInfos[13] = new LibraryBookInfo(1, "100-0000000013", "Book 13",false, "");
+        libraryBookInfos[14] = new LibraryBookInfo(1, "100-0000000014", "Book 14",false, "");
+        libraryBookInfos[15] = new LibraryBookInfo(1, "100-0000000015", "Book 15",false, "");
+        libraryBookInfos[16] = new LibraryBookInfo(1, "100-0000000016", "Book 16",false, "");
+        libraryBookInfos[17] = new LibraryBookInfo(1, "100-0000000017", "Book 17",false, "");
+        libraryBookInfos[18] = new LibraryBookInfo(1, "100-0000000018", "Book 18",false, "");
+        libraryBookInfos[19] = new LibraryBookInfo(1, "100-0000000019", "Book 19",false, "");
+        libraryBookInfos[20] = new LibraryBookInfo(1, "100-0000000020", "Book 20",false, "");
+
+
+    }
+}
