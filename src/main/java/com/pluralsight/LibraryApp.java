e723ef17a1be4ca39cb61a2cd35714ae855dc80d
TheRealRemo <timothyprice2001@gmail.com>
2026-04-18 19:48:53 -0400

    added user prompting with exit statement

--- src/main/java/com/pluralsight/LibraryApp.java
+++ src/main/java/com/pluralsight/LibraryApp.java
@@ -1,30 +1,51 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class LibraryApp {
     private static LibraryBookInfo[] libraryBookInfos = new LibraryBookInfo[20];
     public static void main(String[] args) {
 
-        libraryBookInfos[1] = new LibraryBookInfo(1, "100-0000000001", "Book 1",false, "");
-        libraryBookInfos[2] = new LibraryBookInfo(1, "100-0000000002", "Book 2",false, "");
-        libraryBookInfos[3] = new LibraryBookInfo(1, "100-0000000003", "Book 3",false, "");
-        libraryBookInfos[4] = new LibraryBookInfo(1, "100-0000000004", "Book 4",false, "");
-        libraryBookInfos[5] = new LibraryBookInfo(1, "100-0000000005", "Book 5",false, "");
-        libraryBookInfos[6] = new LibraryBookInfo(1, "100-0000000006", "Book 6",false, "");
-        libraryBookInfos[7] = new LibraryBookInfo(1, "100-0000000007", "Book 7",false, "");
-        libraryBookInfos[8] = new LibraryBookInfo(1, "100-0000000008", "Book 8",false, "");
-        libraryBookInfos[9] = new LibraryBookInfo(1, "100-0000000009", "Book 9",false, "");
-        libraryBookInfos[10] = new LibraryBookInfo(1, "100-0000000010", "Book 10",false, "");
-        libraryBookInfos[11] = new LibraryBookInfo(1, "100-0000000011", "Book 11",false, "");
-        libraryBookInfos[12] = new LibraryBookInfo(1, "100-0000000012", "Book 12",false, "");
-        libraryBookInfos[13] = new LibraryBookInfo(1, "100-0000000013", "Book 13",false, "");
-        libraryBookInfos[14] = new LibraryBookInfo(1, "100-0000000014", "Book 14",false, "");
-        libraryBookInfos[15] = new LibraryBookInfo(1, "100-0000000015", "Book 15",false, "");
-        libraryBookInfos[16] = new LibraryBookInfo(1, "100-0000000016", "Book 16",false, "");
-        libraryBookInfos[17] = new LibraryBookInfo(1, "100-0000000017", "Book 17",false, "");
-        libraryBookInfos[18] = new LibraryBookInfo(1, "100-0000000018", "Book 18",false, "");
-        libraryBookInfos[19] = new LibraryBookInfo(1, "100-0000000019", "Book 19",false, "");
-        libraryBookInfos[20] = new LibraryBookInfo(1, "100-0000000020", "Book 20",false, "");
+        libraryBookInfos[0] = new LibraryBookInfo(1, "100-0000000001", "Book 1",false, "");
+        libraryBookInfos[1] = new LibraryBookInfo(1, "100-0000000002", "Book 2",false, "");
+        libraryBookInfos[2] = new LibraryBookInfo(1, "100-0000000003", "Book 3",false, "");
+        libraryBookInfos[3] = new LibraryBookInfo(1, "100-0000000004", "Book 4",false, "");
+        libraryBookInfos[4] = new LibraryBookInfo(1, "100-0000000005", "Book 5",false, "");
+        libraryBookInfos[5] = new LibraryBookInfo(1, "100-0000000006", "Book 6",false, "");
+        libraryBookInfos[6] = new LibraryBookInfo(1, "100-0000000007", "Book 7",false, "");
+        libraryBookInfos[7] = new LibraryBookInfo(1, "100-0000000008", "Book 8",false, "");
+        libraryBookInfos[8] = new LibraryBookInfo(1, "100-0000000009", "Book 9",false, "");
+        libraryBookInfos[9] = new LibraryBookInfo(1, "100-0000000010", "Book 10",false, "");
+        libraryBookInfos[10] = new LibraryBookInfo(1, "100-0000000011", "Book 11",false, "");
+        libraryBookInfos[11] = new LibraryBookInfo(1, "100-0000000012", "Book 12",false, "");
+        libraryBookInfos[12] = new LibraryBookInfo(1, "100-0000000013", "Book 13",false, "");
+        libraryBookInfos[13] = new LibraryBookInfo(1, "100-0000000014", "Book 14",false, "");
+        libraryBookInfos[14] = new LibraryBookInfo(1, "100-0000000015", "Book 15",false, "");
+        libraryBookInfos[15] = new LibraryBookInfo(1, "100-0000000016", "Book 16",false, "");
+        libraryBookInfos[16] = new LibraryBookInfo(1, "100-0000000017", "Book 17",false, "");
+        libraryBookInfos[17] = new LibraryBookInfo(1, "100-0000000018", "Book 18",false, "");
+        libraryBookInfos[18] = new LibraryBookInfo(1, "100-0000000019", "Book 19",false, "");
+        libraryBookInfos[19] = new LibraryBookInfo(1, "100-0000000020", "Book 20",false, "");
 
+        //Scanner
+        Scanner scanner = new Scanner(System.in);
+        //While Loop for Application prompts
+        boolean isDone = false;
 
-    }
-}
+        while(!isDone){
+            System.out.println("Welcome to the Neighborhood Library!\n");
+            System.out.println("===========================");
+            System.out.println("1 - Show Available Books");
+            System.out.println("2 - Show Checked Out Books");
+            System.out.println("0 - Exit Application");
+            System.out.println("===========================\n");
+            System.out.print("Enter your choice: ");
+
+           int command = scanner.nextInt();
+
+           if (command == 0){
+               isDone = true;
+               System.out.println("\nGoodbye!");
+
+           }
+}}}
